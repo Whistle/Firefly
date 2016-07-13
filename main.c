6a5c4669f035453cb4a852485df674c20a0dc7e1
Dominic Prass <dominic.prass@googlemail.com>
2016-07-13 15:57:49 +0200

    Correction of misleading comment

--- main.c
+++ main.c
@@ -180,7 +180,7 @@ int main() {
 			if(firefly_tokens > 0) {
 				firefly_tokens--;
 
-				// Pull PB4 to GND for PWM
+				// Tie PB4 to VCC for PWM
 				DDRB |= (1<<DDB4);
 
 				PRR &= ~(1<<PRTIM0);
